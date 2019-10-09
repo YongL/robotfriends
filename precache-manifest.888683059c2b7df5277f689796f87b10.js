c346b759f3086ab668f735e1f71226195807e176
yong <souyong2008@gmail.com>
2019-10-09 08:13:28 -0700

    Updates

---
+++ precache-manifest.888683059c2b7df5277f689796f87b10.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29092e67ea67d8395bb9944456e41e24",
+    "url": "/robotfriends/index.html"
+  },
+  {
+    "revision": "e99c0b17ec7899fb49ea",
+    "url": "/robotfriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "c3d6d1647624e865e56e",
+    "url": "/robotfriends/static/css/main.dc395642.chunk.css"
+  },
+  {
+    "revision": "e99c0b17ec7899fb49ea",
+    "url": "/robotfriends/static/js/2.02b613a8.chunk.js"
+  },
+  {
+    "revision": "c3d6d1647624e865e56e",
+    "url": "/robotfriends/static/js/main.b18fe8d4.chunk.js"
+  },
+  {
+    "revision": "0ba0614c7065f28a8bc1",
+    "url": "/robotfriends/static/js/runtime~main.faf9c61a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotfriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
